42f660aefa709955ccb678b569c160307e35b4f7
Tom Scanlan <tompscanlan@gmail.com>
2023-07-28 16:41:23 -0400

    turn off ssr in amplify

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,4 +1,5 @@
 import Amplify from 'aws-amplify'
 import '@aws-amplify/ui-vue';
 import config from '../src/aws-exports'
-Amplify.configure({...config, ssr: true})
+// Amplify.configure({...config, ssr: true})
+Amplify.configure(config)
